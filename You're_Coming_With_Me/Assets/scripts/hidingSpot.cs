e11b1f8a4f2ca9ba430bff5fab4a99b05294ee9e
ahmedrage <ahmedrage@hotmail.com>
2016-03-19 20:20:16 +1030

    Added hiding spot

---
+++ You're_Coming_With_Me/Assets/scripts/hidingSpot.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class hidingSpot : MonoBehaviour {
+	public SpriteRenderer sprite;
+	public Color transparentColor;
+	public Color normalColor;
+
+	public bool isHiding;
+	// Use this for initialization
+	void Start () {
+		sprite = GetComponent<SpriteRenderer> ();
+		normalColor = sprite.color;
+	}
+	
+	// Update is called once per frame
+	void OnTriggerExit2D(Collider2D other) {
+		sprite.color = normalColor;
+
+		if (other.gameObject.tag == "Player") {
+			other.gameObject.layer = 8;
+		}
+	}
+
+	void OnTriggerStay2D(Collider2D other) {
+		sprite.color = transparentColor;
+
+		if (other.gameObject.tag == "Player") {
+			other.gameObject.layer = 9;
+		}
+	}
+}
