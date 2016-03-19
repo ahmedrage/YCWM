78620fef1b039f869f97c1657b5734dced09882d
HIVAladeen <lachiecs@hotmail.com>
2016-03-19 19:22:35 +1030

    enemy started

---
+++ You're_Coming_With_Me/Assets/enemy.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemy : MonoBehaviour {
+
+	public Transform sightStart;
+	public Transform sightEnd;
+	public Transform leader;
+	public float speed;
+	public bool switching;
+	public bool seen;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void Raycasting ()
+	{
+		
+	}
+}
