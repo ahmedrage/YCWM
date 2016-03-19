45e47a30ca303b07537063b3c0548f4713fbf469
ahmedrage <ahmedrage@hotmail.com>
2016-03-19 19:20:11 +1030

    Added player horizontal controls

---
+++ You're_Coming_With_Me/Assets/scripts/playerControls.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerControls : MonoBehaviour {
+	public float speed;
+	public float horizontalMove;
+
+	public Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+	
+		rb = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		horizontalMove = Input.GetAxis ("Horizontal");
+		rb.velocity = new Vector2 (horizontalMove * speed, rb.velocity.y);
+			
+	}
+}
